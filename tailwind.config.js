9a2a904e2dc433c4f2513c26f4a254eaa30d8b53
pabloandrei <pabloandrei0906@gmailcom.br>
2023-09-05 13:20:26 -0300

    Create components for index

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '282': '282px',
+      },
+      height: {
+        '90': '90px',
+        '69.87': '69.87px'
+      },
+      colors: { 
+        'pastel-yellow': '#F4D473',
+        'mustard-yellow': '#F2C029',
+        'olive-yellow': ' #BF9821',
+        'light gray': '#EAEAEA'
+      },
+      fontFamily: {
+        body: ['Palanquin Dark']
+      },
+      boxShadow: {
+        'custom': '0 5px 10px 0 rgba(0, 0, 0, 0.5)'
+      },
+    },
   },
   plugins: [],
 };
+
+
