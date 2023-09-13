2ea8595a269393682b786bd115075c78fc50d64c
pabloandrei <pabloandrei0906@gmailcom.br>
2023-09-13 07:45:38 -0300

    adicionado ícone dark-mode, e baixado a biblioteca daisyUI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { ButtonComponent } from './component/button/button.component';
+import { BannerComponent } from './component/banner/banner.component';
 
 const routes: Routes = [
-  {path:'nav-bar', component:NavBarComponent}, 
+  {path:'nav-bar', component: NavBarComponent},
+  {path:'button', component: ButtonComponent},
+  {path:'banner', component: BannerComponent},
 ];
 
 @NgModule({
