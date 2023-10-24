06c2f520ffff7c69b48530f410de4a874b1b935e
Adnir Andrade <adnir_andrade@hotmail.com>
2023-10-24 19:51:57 -0300

    Refactores path from software to form

--- frontend/src/app/component/software-list/software-list.component.ts
+++ frontend/src/app/component/software-list/software-list.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { BlockService } from 'src/app/block.service';
+import { Software } from 'src/app/software';
 
 @Component({
   selector: 'app-software-list',
   templateUrl: './software-list.component.html',
-  styleUrls: ['./software-list.component.css']
+  styleUrls: ['./software-list.component.css'],
 })
 export class SoftwareListComponent {
+  softwares$: Observable<Software[]> = new Observable();
+  blockId: string = '';
+  labId: string = '';
 
+  constructor(
+    private blockService: BlockService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.blockId = params['blockId'];
+      this.labId = params['labId'];
+    });
+  }
+
+  navigateToForm(): void {
+    this.router.navigate([
+      'blocos',
+      this.blockId,
+      'labs',
+      this.labId,
+      'softwares',
+      'form',
+    ]);
+  }
 }
